ad682a94700dba7b81ad247f5249d78145dc3817
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2026-03-15 11:51:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-apk update && apk add zram-init crun docker && \
+apk update && \
+apk add zram-init crun docker unzip nano screen jq && \
 mkdir -p /etc/docker && \
 cat <<EOF > /etc/conf.d/zram-init
 num_devices=1
@@ -24,6 +25,18 @@ fs.file-max=2097152
 vm.max_map_count=262144
 EOF
 sysctl -p /etc/sysctl.d/99-thousands-containers.conf && \
+
+modprobe tun && \
+sysctl -w fs.inotify.max_user_watches=4194304 && \
+sysctl -w fs.inotify.max_user_instances=8192 && \
+sysctl -w fs.inotify.max_queued_events=65536 && \
+sysctl fs.inotify && \
+sysctl -w net.ipv4.neigh.default.gc_thresh1=1024 && \
+sysctl -w net.ipv4.neigh.default.gc_thresh2=4096 && \
+sysctl -w net.ipv4.neigh.default.gc_thresh3=8192 && \
+rc-service docker start && \
+rc-update add docker boot && \
+docker --version && \
 rc-update add zram-init default && \
 rc-update add docker default && \
 rc-service zram-init restart && \
